da9ff363a8bf16a242f6ba0368a8f09a622d3de0
griffeyTheGr8 <42072301+griffeyTheGr8@users.noreply.github.com>
2020-02-21 12:22:45 -0600

    add script via upload
    Added the Python script for the tic tac toe game.

---
+++ ticTacToe.py
@@ -0,0 +1,29 @@
+"""
+ticTacToe.py
+Ch5 Dictionaries
+Using Data Structures to Model Real-World Things
+"""
+
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+turn = 'X'
+
+for i in range(9):
+    printBoard(theBoard)
+    move = input(turn + '\'s turn. What place do you want to claim: ')
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+
+printBoard(theBoard)
